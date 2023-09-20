7094275c783a97188a0f8ea8c8aecebaf1f4116d
Ayushi-gupta2001 <g.dibs2001@gmail.com>
2023-09-20 14:11:19 +0530

    carasoul implementation

---
+++ FrontEnd/src/app/carasool/carasool.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carasool',
+  templateUrl: './carasool.component.html',
+  styleUrls: ['./carasool.component.scss']
+})
+export class CarasoolComponent {
+
+}
