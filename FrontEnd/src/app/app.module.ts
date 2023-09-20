df50e2768106cc7c2ee4943ccf05dadf89a0ecbc
Ayushi-gupta2001 <g.dibs2001@gmail.com>
2023-09-20 14:09:41 +0530

    adding selector

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarasoolComponent } from './carasool/carasool.component';
+import { CardbodyComponent } from './cardbody/cardbody.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarasoolComponent,
+    CardbodyComponent
   ],
   imports: [
     BrowserModule,
