0e73ab61d800785d08ccbc64798bb39c033fe8a9
Aditya-Bhardwaj1 <adityabhardwaj2722@gmail.com>
2023-09-20 12:12:55 +0530

    Updated tables, Added getallcategory, addcategory, updateproduct webapis

--- Grocery.Soti.Project/Grocery.Soti.Project.DAL/Models/Product.cs
+++ Grocery.Soti.Project/Grocery.Soti.Project.DAL/Models/Product.cs
@@ -15,7 +15,7 @@ namespace Grocery.Soti.Project.DAL.Models
         public string Description { get; set; }
         public decimal UnitPrice { get; set; }
         public int UnitsInStock { get; set; }
-        public bool Discontinued { get; set; }
+        public Boolean Discontinued { get; set; }
         public int CategoryId { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? ModifiedDate { get; set; }
