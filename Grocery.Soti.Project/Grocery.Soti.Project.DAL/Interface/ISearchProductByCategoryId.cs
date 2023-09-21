42c5cc9572f3292f814dc7681afe1bfa4dbcaa7d
Ayushi-gupta2001 <g.dibs2001@gmail.com>
2023-09-21 15:14:08 +0530

    Interface for search product by categoryId

---
+++ Grocery.Soti.Project/Grocery.Soti.Project.DAL/Interface/ISearchProductByCategoryId.cs
@@ -0,0 +1,16 @@
+﻿using SOTI.DAL.DEMO.SEARCH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOTI.DAL.DEMO.SEARCH.Interface
+{
+    public interface ISearchProductByCategoryId
+    {
+       // object GetProduct();
+
+        List<Product> SearchProductByCategoryId(int CategoryId);
+    }
+}
