0d20df62950addd18b1a15664597fda414d40fb7
Ayushi-gupta2001 <g.dibs2001@gmail.com>
2023-09-21 19:04:51 +0530

    app component

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarasoolComponent } from './carasool/carasool.component';
+import { CardbodyComponent } from './cardbody/cardbody.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
+const appRoutes: Routes = [
+  //{ path: '', component: NavbarComponent} // localhost:8000
+  //{path: 'login', component: logincomponnt}
+  //{path : 'products', component: productcomponent}
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarasoolComponent,
+    CardbodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
