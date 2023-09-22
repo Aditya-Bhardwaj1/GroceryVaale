0d43fa4e40f18dfbcd94749fe736d3f074d5499d
Ayushi-gupta2001 <g.dibs2001@gmail.com>
2023-09-22 16:15:13 +0530

    adding function for list products for category Id

--- Grocery.Soti.Project/Grocery.Soti.Project.DAL/ProductDetails.cs
+++ Grocery.Soti.Project/Grocery.Soti.Project.DAL/ProductDetails.cs
@@ -81,5 +81,36 @@ namespace Grocery.Soti.Project.DAL
                 }
             }
         }
+
+        // search product by category Id
+
+        public List<Product> ListProducts(int categoryId)
+        {
+            using (_connection = new SqlConnection(SqlConnectionString.GetConnectionString))
+            {
+                using (_adapter = new SqlDataAdapter("select * from Products ", _connection))
+                {
+                    using (_dataset = new DataSet())
+                    {
+                        _adapter.Fill(_dataset, "Products");
+
+                        var product = _dataset.Tables["Products"].AsEnumerable()
+                                     .Select(x => new Product
+                                     {
+                                         ProductName = x.Field<string>("ProductName"),
+                                         Description = x.Field<string>("Description"),
+                                         UnitPrice = x.Field<decimal>("UnitPrice"),
+                                         UnitsInStock = x.Field<int>("UnitsInStock"),
+                                         CategoryId = x.Field<int>("CategoryId"),
+                                     }).Where(x => x.CategoryId == categoryId).ToList();
+
+                        return product;
+
+                    }
+                }
+
+            }
+
+        }
     }
 }
