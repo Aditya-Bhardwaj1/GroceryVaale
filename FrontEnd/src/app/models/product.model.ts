024fa86f52a5a075d511d136a4d0ca5784f746ff
Ayushi-gupta2001 <g.dibs2001@gmail.com>
2023-09-22 16:07:02 +0530

    model for product and category services

---
+++ FrontEnd/src/app/models/product.model.ts
@@ -0,0 +1,21 @@
+export class Product {
+productId?: number;
+    
+        productName?: string ;
+    
+        description?: string;
+    
+        unitPrice? : number;
+    
+        unitsInStock?:number;
+    
+        dicontinued?:boolean;
+    
+        categoryId?:number;
+    
+        createdDate? : Date;
+    
+        modifiedDate?:Date;
+    
+        productImage?:string;
+}
